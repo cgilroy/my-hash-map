8254a535c6d077770796641665e398b9e7abd480
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-05-24 17:22:26 -0600

    Add ResizingIntSet

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -72,18 +72,29 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+    if !self.include?(num)
+      self[num] << num 
+      @count += 1
+    end
   end
 
   def remove(num)
+    if self.include?(num)
+      self[num].delete(num)
+      @count += -1
+    end
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -91,5 +102,14 @@ class ResizingIntSet
   end
 
   def resize!
+    old_store = @store
+    @store = Array.new(num_buckets*2) { Array.new }
+
+    old_store.each do |bucket|
+      bucket.each do |el|
+        self.insert(el)
+        @count += -1
+      end
+    end
   end
 end
