c35b8cc7fea42d171f899c27b79c1e7826b1dead
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-05-24 14:55:47 -0600

    Initial commit (MaxIntSet done)

---
+++ lib/p01_int_set.rb
@@ -0,0 +1,91 @@
+require 'byebug'
+class MaxIntSet
+  attr_accessor :store
+  def initialize(max)
+    @store = Array.new(max) {false}
+  end
+
+  def insert(num)
+    validate!(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    validate!(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    validate!(num)
+    return true if @store[num] == true
+    false
+  end
+
+  private
+
+  def is_valid?(num)
+    return false if num < 0 || num > @store.length-1
+    true
+  end
+
+  def validate!(num)
+    raise "Out of bounds" if !is_valid?(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+  end
+
+  def remove(num)
+  end
+
+  def include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+  end
+end
