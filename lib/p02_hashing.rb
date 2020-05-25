e045df39b8351b9fa35bc920fdb85a3dea1cb5d0
CJ Gilroy <c.gilroy9@gmail.com>
2020-05-25 08:24:57 -0600

    Write custom hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    hash_sum = 0
+    self.each_with_index do |el,idx|
+      hash_sum += el.hash * idx.hash
+    end
+    hash_sum.hash
   end
 end
 
 class String
   def hash
+    hash_sum = 0
+    self.chars.each_with_index do |char,idx|
+      hash_sum += char.ord.hash * idx.hash 
+    end
+    hash_sum.hash
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    hash_sum = 0
+    self.each do |k,v|
+      hash_sum += k.hash * v.hash
+    end
+    hash_sum.hash
   end
 end
