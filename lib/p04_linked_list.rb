02ab1e65ff5e0c18cd9f4483ae2d3065f2c4c69d
CJ Gilroy <cjgilroy@MacBook-Pro.local>
2020-05-25 20:24:47 -0600

    Added LinkedList class

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+
+require 'byebug'
 class Node
   attr_reader :key
   attr_accessor :val, :next, :prev
@@ -51,32 +53,56 @@ class LinkedList
   end
 
   def get(key)
+    self.each do |node|
+      if node.key == key
+        return node.val
+      end
+    end
+    nil
   end
 
   def include?(key)
+    return true if !get(key).nil?
+    false
   end
 
   def append(key, val)
     new_node = Node.new(key,val)
-    @head.next = new_node
+    old_last_node = @tail.prev
+    new_node.prev = old_last_node
+    new_node.next = @tail
+    old_last_node.next = new_node
+    @tail.prev = new_node
   end
 
   def update(key, val)
+    self.each do |node|
+      node.val = val if node.key == key
+    end
   end
 
   def remove(key)
+    self.each do |node|
+      if node.key == key
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
+      end
+    end
   end
 
   def each
-    current_node = @head
-    until current_node.nil?
+    # debugger
+    current_node = @head.next
+    until current_node == @tail
       yield(current_node)
       current_node = current_node.next
     end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
