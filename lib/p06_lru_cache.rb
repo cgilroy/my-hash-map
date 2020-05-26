935e03af5c7e6b4a76fc92b04f9c6f761b58327d
CJ Gilroy <c.gilroy9@gmail.com>
2020-05-26 14:09:15 -0600

    Add custom LRUCache class

--- lib/p06_lru_cache.rb
+++ lib/p06_lru_cache.rb
@@ -2,6 +2,7 @@ require_relative 'p05_hash_map'
 require_relative 'p04_linked_list'
 
 class LRUCache
+  attr_reader :store, :map
   def initialize(max, prc)
     @map = HashMap.new
     @store = LinkedList.new
@@ -14,6 +15,15 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      update_node!(@map[key])
+      return @map[key].val
+    else
+      new_node = calc!(key)
+      @map.set(key,new_node)
+      eject! if count > @max
+      new_node.val
+    end
   end
 
   def to_s
@@ -24,12 +34,18 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    @store.append(key,@prc.call(key))
   end
 
   def update_node!(node)
     # suggested helper method; move a node to the end of the list
+    @store.remove(node.key)
+    new_node = @store.append(node.key,node.val)
+    @map.set(node.key,new_node)
   end
 
   def eject!
+    @map.delete(@store.first.key)
+    @store.remove(@store.first.key)
   end
 end
